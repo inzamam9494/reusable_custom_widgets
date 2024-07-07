3ae47e753bcd757693dd50ea0a6195a9343bcfb7
mohdi <mohdinzamam12125@gmail.com>
2024-07-07 10:44:02 +0530

    custom colors

---
+++ lib/colors/gradiant_color.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+const orangeGradient = LinearGradient(
+  colors: [
+    Color(0xFFF43F5E), // Rose
+
+    Color(0xFF0EA5E9), // Sky
+    Color(0xFFF97316), // Orange
+  ],
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+);
+
+const blueGradient = LinearGradient(
+  colors: [
+    Color(0xFF380331), // Rose
+
+    Color(0xFFA90FAF), // Sky
+    Color(0xFF380331), // Orange
+  ],
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+);
+
+
+const primaryGradientColor = LinearGradient(
+  colors: [
+    Color(0xFFa152cd),
+    Color(0xFFe1654d),
+
+  ],
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+);
+
+const greyGradientColor = LinearGradient(
+  colors: [
+    Colors.grey,
+    Colors.grey
+// Color(0xFFe1654d),
+
+  ],
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+);
