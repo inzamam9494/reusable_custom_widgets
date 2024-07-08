415cf403ec19b6fc6a748cd1d190836bff04d7ce
mohdi <mohdinzamam12125@gmail.com>
2024-07-08 22:50:54 +0530

    text style

---
+++ lib/style/text_style.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../colors/color.dart';
+
+class AppTextStyles {
+  static TextStyle kLargeTitleTextStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 48.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kHeading1TextStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 36.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kHeading2TextStyle = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 32.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kHeading2RegularTextStyle = TextStyle(
+    fontSize: 32.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kHeading3TextStyle = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 28.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kHeading3RegularTextStyle = TextStyle(
+    fontSize: 28.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kBody20RegularTextStyle = TextStyle(
+    fontSize: 24.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kBody20SemiBoldTextStyle = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 24.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kBody17RegularTextStyle = TextStyle(
+    fontSize: 20.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kBody17SemiBoldTextStyle = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 20.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kBody15RegularTextStyle = TextStyle(
+    fontSize: 18.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kBody15SemiBoldTextStyle = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 18.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kCaption14RegularTextStyle = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 16.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kCaption12RegularTextStyle = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 14.sp, // Larger font size for desktop/laptop
+  );
+  static TextStyle kCaption12WRegularTextStyle = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 14.sp, // Larger font size for desktop/laptop
+  );
+  static TextStyle kCaption12SemiBoldTextStyle = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 14.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kSmall10RegularTextStyle = TextStyle(
+    fontSize: 12.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kSmall10SemiBoldTextStyle = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 12.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kSmall10SemiBoldTextStyleWhite = TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 12.sp,
+    // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kSmall8RegularTextStyle = TextStyle(
+    fontSize: 10.sp,// Larger font size for desktop/laptop
+  );
+
+  static TextStyle kSmall8RegularTextStyleGrey = TextStyle(
+      fontSize: 10.sp,// Larger font size for desktop/laptop
+      color: Colors.grey
+  );
+
+  static TextStyle kSmall8SemiBoldTextStyleWhite = TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 10.sp,
+      color: AppColors.white// Larger font size for desktop/laptop
+  );
+
+  static TextStyle kSmall8SemiBoldTextStyle = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 10.sp,
+    // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kSmall7RegularTextStyle = TextStyle(
+    fontSize: 8.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kSmall6RegularTextStyle = TextStyle(
+    fontSize: 6.sp, // Larger font size for desktop/laptop
+  );
+
+  static TextStyle kSmall4RegularTextStyle = TextStyle(
+    fontSize: 4.sp, // Larger font size for desktop/laptop
+  );
+
+  //todo Create Customized fonts
+
+}
